1713df455160d6e1b81b8d400b53641d090240f1
Ryzen <vithor.norberto@gmail.com>
2018-10-10 13:19:56 -0300

    V.0.1.3
    final 1

--- Lavanderia/main.c
+++ Lavanderia/main.c
@@ -68,64 +68,64 @@ int main()
                 fputs (""__DATE__" "__TIME__"\n", p);
                 fflush(stdin);
 
-                printf("\nDigite o nome do cliente: ");
+                printf("\nDigite o nome do cliente:  ");
                 scanf("%[^\n]s",C.nome);
                 fprintf(p,"Nome do cliente: -------------------------- %s\n",C.nome);
                 fflush(stdin);
 
-                printf("Digite o telefone do cliente: ");
+                printf("Digite o telefone do cliente:  ");
                 scanf("%s",C.telefone);
                 fprintf(p,"Telefone do cliente: ---------------------- %s\n",C.telefone);
                 fflush(stdin);
 
-                printf("Digite o Endereço do cliente: ");
+                printf("Digite o Endereço do cliente:  ");
                 scanf("%[^\n]s",C.endereco);
                 fprintf(p,"Endereço do cliente: ---------------------- %s\n",C.endereco);
                 fflush(stdin);
 
-                printf("Digite o peso da roupa do cliente: ");
+                printf("Digite o peso da roupa do cliente:  ");
                 scanf("%lf",&C.peso_roupa);
                 C.valor = C.peso_roupa * 12;
                 fprintf(p,"Peso da roupa do cliente: ----------------- %.2lf KG\n",C.peso_roupa);
                 fflush(stdin);
 
-                printf("Digite a quantidade de Edredom de Solteiro: ");
+                printf("Digite a quantidade de Edredom de Solteiro:  ");
                 scanf("%lf",&C.edredomSolteiro);
                 C.valor += C.edredomSolteiro * 15;
                 fprintf(p,"Edredom de Solteiro: ---------------------- %.2lf UNI\n",C.edredomSolteiro);
                 fflush(stdin);
 
-                printf("Digite a quantidade de Edredom de Casal: ");
+                printf("Digite a quantidade de Edredom de Casal:  ");
                 scanf("%lf",&C.edredomCasal);
                 C.valor += C.edredomCasal * 20;
                 fprintf(p,"Edredom de Casal: ------------------------- %.2lf UNI\n",C.edredomCasal);
                 fflush(stdin);
 
-                printf("Digite a quantidade de paletó e blazer: ");
+                printf("Digite a quantidade de paletó e blazer:  ");
                 scanf("%lf",&C.paletoBlazer);
                 C.valor += C.paletoBlazer * 15;
                 fprintf(p,"Paleto e Blazer: -------------------------- %.2lf UNI\n",C.paletoBlazer);
                 fflush(stdin);
 
-                printf("Digite a quantidade de paletós completos: ");
+                printf("Digite a quantidade de paletós completos:  ");
                 scanf("%lf",&C.paletoCompleto);
                  C.valor += C.paletoCompleto * 20;
                 fprintf(p,"Paletos Completos: ------------------------ %.2lf UNI\n",C.paletoCompleto);
                 fflush(stdin);
 
-                printf("Digite a quantidade de Tapetes: ");
+                printf("Digite a quantidade de Tapetes:  ");
                 scanf("%lf",&C.tapete);
                 C.valor += C.tapete * 15;
                 fprintf(p,"Tapetes: ---------------------------------- %.2lf M²\n",C.tapete);
                 fflush(stdin);
 
-                printf("Digite a quantidade de Passadeira de chão/mesa Pequena: ");
+                printf("Quantidade de Passadeira de chão/mesa Pequena: ");
                 scanf("%lf",&C.passadeiraPeq);
                 C.valor += C.passadeiraPeq * 15;
                 fprintf(p,"Passadeira de chão/mesa peq --------------- %.2lf UNI\n",C.passadeiraPeq);
                 fflush(stdin);
 
-                printf("Digite a quantidade de Passadeira de chão/mesa Grande: ");
+                printf("Quantidade de Passadeira de chão/mesa Grande: ");
                 scanf("%lf",&C.passadeiraGran);
                 C.valor += C.passadeiraGran * 20;
                 fprintf(p,"Passadeira de chão/mesa Grande: ----------- %.2lf UNI\n",C.passadeiraGran);
@@ -134,7 +134,6 @@ int main()
 
                 printf("Digite a quantidade de Vestidos de Festa: ");
                 scanf("%lf",&C.vestido);
-                printf("Digite o Valor de cada Vestido de Festa: \n");
                 int tam = C.vestido;
                 for(i = 0; i < tam; i++){
                     printf("Valor do Vestido de Festa %d: ",i+1);
@@ -157,13 +156,13 @@ int main()
                 fprintf(p,"Vestidos Infantis: ------------------------ %.2lf UNI\n",C.vestidoInf);
                 fflush(stdin);
 
-                printf("Digite a quantidade de Camisas Socias de Mangas Longas: ");
+                printf("Quantidade de Camisas Socias de Mangas Longas: ");
                 scanf("%lf",&C.camisaSocialML);
                 fprintf(p,"Camisas Socias de Mangas Longas: ---------- %.2lf UNI\n",C.camisaSocialML);
                 C.valor += C.camisaSocialML * 10;
                 fflush(stdin);
 
-                printf("Digite a quantidade de Camisas Socias de Mangas Curtas: ");
+                printf("Quantidade de Camisas Socias de Mangas Curtas: ");
                 scanf("%lf",&C.camisaSocialMC);
                 C.valor += C.camisaSocialMC * 7;
                 fprintf(p,"Camisas Socias de Mangas Curtas: ---------- %.2lf UNI\n",C.camisaSocialMC);
